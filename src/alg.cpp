d86759fb40466c3028e116493e04c9828db784e0
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-05-10 17:51:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,77 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int getPriority(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    } else {
+        return 0;
+    }
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+string infixToPostfix(string infix) {
+    stack<char> opStack;
+    string postfix = "";
+
+    for (int i = 0; i < infix.length(); i++) {
+        char c = infix[i];
+        if (isdigit(c)) {
+            postfix += c;
+        } else if (c == '(') {
+            opStack.push(c);
+        } else if (c == ')') {
+            while (opStack.top() != '(') {
+                postfix += opStack.top();
+                opStack.pop();
+            }
+            opStack.pop();
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            while (!opStack.empty() && getPriority(opStack.top()) >= getPriority(c)) {
+                postfix += opStack.top();
+                opStack.pop();
+            }
+            opStack.push(c);
+        }
+    }
+
+    while (!opStack.empty()) {
+        postfix += opStack.top();
+        opStack.pop();
+    }
+
+    return postfix;
+}
+int evaluatePostfix(string postfix) {
+    stack<int> valStack;
+
+    for (int i = 0; i < postfix.length(); i++) {
+        char c = postfix[i];
+        if (isdigit(c)) {
+            valStack.push(c - '0');
+        } else {
+            int val2 = valStack.top();
+            valStack.pop();
+            int val1 = valStack.top();
+            valStack.pop();
+
+            switch (c) {
+                case '+':
+                    valStack.push(val1 + val2);
+                    break;
+                case '-':
+                    valStack.push(val1 - val2);
+                    break;
+                case '*':
+                    valStack.push(val1 * val2);
+                    break;
+                case '/':
+                    valStack.push(val1 / val2);
+                    break;
+            }
+        }
+    }
+
+    return valStack.top();
 }
