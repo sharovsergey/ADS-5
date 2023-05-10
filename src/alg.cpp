33c64eefeb74bd93f5eeed18db9388010e00d8d5
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-05-10 17:57:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,8 @@
 #include <map>
 #include "tstack.h"
 
+using namespace std;
+
 int getPriority(char op) {
     if (op == '+' || op == '-') {
         return 1;
@@ -44,4 +46,36 @@ string infixToPostfix(string infix) {
     }
 
     return postfix;
+}
+int evaluatePostfix(string postfix) {
+    stack<int> valStack;
+
+    for (int i = 0; i < postfix.length(); i++) {
+        char c = postfix[i];
+        if (isdigit(c)) {
+            valStack.push(c - '0');
+        } else {
+            int val2 = valStack.top();
+            valStack.pop();
+            int val1 = valStack.top();
+            valStack.pop();
+
+            switch (c) {
+                case '+':
+                    valStack.push(val1 + val2);
+                    break;
+                case '-':
+                    valStack.push(val1 - val2);
+                    break;
+                case '*':
+                    valStack.push(val1 * val2);
+                    break;
+                case '/':
+                    valStack.push(val1 / val2);
+                    break;
+            }
+        }
+    }
+
+    return valStack.top();
 }
