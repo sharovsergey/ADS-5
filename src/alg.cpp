8c956a93f7fef519c18e38d369f0787842e1f1ac
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-05-10 17:46:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,48 +3,45 @@
 #include <map>
 #include "tstack.h"
 
-using namespace std;
+int getPriority(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    } else {
+        return 0;
+    }
+}
 
-int evaluatePostfix(string postfix) {
-    stack<int> valStack;
+string infixToPostfix(string infix) {
+    stack<char> opStack;
+    string postfix = "";
 
-    for (int i = 0; i < postfix.length(); i++) {
-        char c = postfix[i];
+    for (int i = 0; i < infix.length(); i++) {
+        char c = infix[i];
         if (isdigit(c)) {
-            valStack.push(c - '0');
-        } else {
-            int val2 = valStack.top();
-            valStack.pop();
-            int val1 = valStack.top();
-            valStack.pop();
-
-            switch (c) {
-                case '+':
-                    valStack.push(val1 + val2);
-                    break;
-                case '-':
-                    valStack.push(val1 - val2);
-                    break;
-                case '*':
-                    valStack.push(val1 * val2);
-                    break;
-                case '/':
-                    valStack.push(val1 / val2);
-                    break;
+            postfix += c;
+        } else if (c == '(') {
+            opStack.push(c);
+        } else if (c == ')') {
+            while (opStack.top() != '(') {
+                postfix += opStack.top();
+                opStack.pop();
             }
+            opStack.pop();
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            while (!opStack.empty() && getPriority(opStack.top()) >= getPriority(c)) {
+                postfix += opStack.top();
+                opStack.pop();
+            }
+            opStack.push(c);
         }
     }
 
-    return valStack.top();
-}
+    while (!opStack.empty()) {
+        postfix += opStack.top();
+        opStack.pop();
+    }
 
-Пример использования:
-
-int main() {
-    string infix = "3+4*2/(1-5)^2";
-    string postfix = infixToPostfix(infix);
-    cout << "Postfix: " << postfix << endl;
-    int result = evaluatePostfix(postfix);
-    cout << "Result: " << result << endl;
-    return 0;
+    return postfix;
 }
