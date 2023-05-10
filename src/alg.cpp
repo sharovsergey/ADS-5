ab434c75d1eb13628327d8b0cf721126c936acd6
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-05-10 17:41:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,48 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+using namespace std;
+
+int evaluatePostfix(string postfix) {
+    stack<int> valStack;
+
+    for (int i = 0; i < postfix.length(); i++) {
+        char c = postfix[i];
+        if (isdigit(c)) {
+            valStack.push(c - '0');
+        } else {
+            int val2 = valStack.top();
+            valStack.pop();
+            int val1 = valStack.top();
+            valStack.pop();
+
+            switch (c) {
+                case '+':
+                    valStack.push(val1 + val2);
+                    break;
+                case '-':
+                    valStack.push(val1 - val2);
+                    break;
+                case '*':
+                    valStack.push(val1 * val2);
+                    break;
+                case '/':
+                    valStack.push(val1 / val2);
+                    break;
+            }
+        }
+    }
+
+    return valStack.top();
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+Пример использования:
+
+int main() {
+    string infix = "3+4*2/(1-5)^2";
+    string postfix = infixToPostfix(infix);
+    cout << "Postfix: " << postfix << endl;
+    int result = evaluatePostfix(postfix);
+    cout << "Result: " << result << endl;
+    return 0;
 }
