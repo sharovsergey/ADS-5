297b22f9fdd14d441550b941f6c90f46d226bc2b
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-05-15 23:52:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,77 +5,94 @@
 
 using namespace std;
 
-int getPriority(char op) {
-    if (op == '+' || op == '-') {
-        return 1;
-    } else if (op == '*' || op == '/') {
-        return 2;
-    } else {
-        return 0;
-    }
+
+bool isOperator(const char &c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
 }
 
-string infixToPostfix(string infix) {
-    stack<char> opStack;
-    string postfix = "";
+bool hasLessOrEqualPriority(const char &op1, const char &op2) {
+    if ((op1 == '+' || op1 == '-') && (op2 == '*' || op2 == '/')) {
+        return true;
+    }
+    return false;
+}
 
-    for (int i = 0; i < infix.length(); i++) {
-        char c = infix[i];
-        if (isdigit(c)) {
-            postfix += c;
-        } else if (c == '(') {
-            opStack.push(c);
+
+string infixToPostfix(const string &infix) {
+    stack<char> s;
+    string postfix;
+
+    for (const char &c : infix) {
+        if (c == '(') {
+            s.push(c);
         } else if (c == ')') {
-            while (opStack.top() != '(') {
-                postfix += opStack.top();
-                opStack.pop();
+            while (!s.empty() && s.top() != '(') {
+                postfix.push_back(s.top());
+                s.pop();
             }
-            opStack.pop();
-        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-            while (!opStack.empty() && getPriority(opStack.top()) >= getPriority(c)) {
-                postfix += opStack.top();
-                opStack.pop();
+            s.pop();
+        } else if (isOperator(c)) {
+            while (!s.empty() && s.top() != '(' && hasLessOrEqualPriority(c, s.top())) {
+                postfix.push_back(s.top());
+                s.pop();
             }
-            opStack.push(c);
+            s.push(c);
+        } else {
+            postfix.push_back(c);
         }
     }
 
-    while (!opStack.empty()) {
-        postfix += opStack.top();
-        opStack.pop();
+    while (!s.empty()) {
+        postfix.push_back(s.top());
+        s.pop();
     }
 
     return postfix;
 }
-int evaluatePostfix(string postfix) {
-    stack<int> valStack;
 
-    for (int i = 0; i < postfix.length(); i++) {
-        char c = postfix[i];
+int main() {
+    string infix = "a+b*c-d/e";
+    string postfix = infixToPostfix(infix);
+    cout << postfix << endl;
+    return 0;
+}
+bool isOperator(const char &c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+double applyOperator(const double &operand1, const double &operand2, const char &op) {
+    if (op == '+') {
+        return operand1 + operand2;
+    } else if (op == '-') {
+        return operand1 - operand2;
+    } else if (op == '*') {
+        return operand1 * operand2;
+    } else if (op == '/') {
+        return operand1 / operand2;
+    }
+    return 0;
+}
+double evaluatePostfix(const string &postfix) {
+    stack<double> s;
+    double operand1, operand2;
+
+    for (const char &c : postfix) {
         if (isdigit(c)) {
-            valStack.push(c - '0');
-        } else {
-            int val2 = valStack.top();
-            valStack.pop();
-            int val1 = valStack.top();
-            valStack.pop();
-
-            switch (c) {
-                case '+':
-                    valStack.push(val1 + val2);
-                    break;
-                case '-':
-                    valStack.push(val1 - val2);
-                    break;
-                case '*':
-                    valStack.push(val1 * val2);
-                    break;
-                case '/':
-                    valStack.push(val1 / val2);
-                    break;
-            }
+            s.push((double)(c - '0'));
+        } else if (isOperator(c)) {
+            operand2 = s.top();
+            s.pop();
+            operand1 = s.top();
+            s.pop();
+            s.push(applyOperator(operand1, operand2, c));
         }
     }
 
-    return valStack.top();
+    return s.top();
+}
+
+int main() {
+    string postfix = "abc*+de/-";
+    double result = evaluatePostfix(postfix);
+    cout << result << endl;
+    return 0;
 }
