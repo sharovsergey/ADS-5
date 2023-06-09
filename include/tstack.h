279a482c6f94ba619eebe7ea94f26118f36e3188
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-06-10 00:16:15 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,45 @@
 // Copyright 2021 NNTU-CS
+// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
-class TStack {
-  // добавьте код стека
-};
+#include <string>
 
+template <typename T, int size>
+class Tstack {
+ private:
+  T arr[100];
+  int top;
+
+ public:
+  Tstack() :top(-1) {}
+
+  void push(T value) {
+    if (isFull()) {
+      throw std::string("Stack is full");
+    } else {
+      arr[++top] = value;
+      }
+    }
+
+  const T& pop() {
+    if (isEmpty()) {
+      throw std::string("Stack is empty");
+    } else {
+      return arr[top--];
+      }
+    }
+
+  bool isEmpty()const {
+    return top == -1;
+  }
+
+  bool isFull()const {
+    return top == size - 1;
+  }
+
+  const T& get()const {
+    return arr[top];
+  }
+};
 #endif  // INCLUDE_TSTACK_H_
