6cff041555bdd92063bd55f2228ea7db47161e4d
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-06-09 23:03:06 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -1,10 +1,44 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_ALG_H_
-#define INCLUDE_ALG_H_
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+
 
 #include <string>
+template<typename T, int size>
+class TStack {
+// добавьте код стека
+private:
+T arr[100];
+int serg;
 
-std::string infx2pstfx(std::string);
-int eval(std::string);
 
-#endif  // INCLUDE_ALG_H_
+public:
+TStack() :serg(-1) {}
+void push(T value) {
+if (isFull()) {
+throw std::string("Stack is full");
+} else {
+arr[++serg] = value;
+}
+}
+const T& pop() {
+if (isEmpty()) {
+throw std::string("Stack is empty");
+} else {
+return arr[serg--];
+}
+}
+bool isEmpty()const {
+return serg == -1;
+}
+bool isFull()const {
+return serg == size - 1;
+}
+const T& get()const {
+return arr[serg];
+}
+};
+
+
+#endif
+ // INCLUDE_TSTACK_H_
