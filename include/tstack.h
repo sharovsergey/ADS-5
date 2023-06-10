1b863e2b68c891f49cb6dce97fdaedd4a7c61896
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-06-10 22:37:50 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,30 +1,45 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-#include <string>
 
+#include <string>
 template<typename T, int size>
-template <typename T, int size>
 class TStack {
   // добавьте код стека
+};
  private:
-  T mass[size] = {0};
-  int a;
+    T arr[100];
+    int m;
 
  public:
-  TStack() { a = -1; }
-  void push(const T& value) {
-    if (full()) {
-      throw std::string("  empty!");
-    } else {
-      mass[++a] = value;
+    TStack() : m(-1) {}
+    void push(T value) {
+        if (!isFull())
+            arr[++m] = value;
+        else
+            throw std::string("Full");
     }
-  }
-  T pol() const { return mass[a]; }
-  T pop() { return mass[a--]; }
-  int pri() const { return a; }
-  bool empty() const { return a == -1; }
-  bool full() const { return a == size; }
+    T get() const {
+        return arr[m];
+    }
+    T pop() {
+        if (isEmpty())
+            throw std::string("Empty");
+        else
+            return arr[m--];
+    }
+    bool isEmpty() const {
+        return m == -1;
+    }
+    bool isFull() const {
+        return m == size - 1;
+    }
+    T top() {
+        if (!isEmpty()) {
+            return arr[m];
+        } else {
+            exit(EXIT_FAILURE);
+        }
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
