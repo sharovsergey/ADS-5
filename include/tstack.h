35a3da26a71801ddc441c01e487d0db196eeb62e
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-06-10 20:12:11 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,48 +1,116 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-
-template<typename T, int size>
-class TStack {
-  // добавьте код стека
-};
+#include <iostream>
 #include <string>
+#include "include/tstack.h"
 
-template <typename T, int size>
-class Tstack {
- private:
-  T arr[100];
-  int top;
 
- public:
-  Tstack() :top(-1) {}
+int priority(char c) {
+    if (c == '*' || c == '/')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return 0;
+}
 
-  void push(T value) {
-    if (isFull()) {
-      throw std::string("Stack is full");
-    } else {
-      arr[++top] = value;
-      }
+
+std::string infx2pstfx(std::string inf) {
+    TStack<char, 100> stack;
+    std::string post;
+    int len = inf.length();
+
+    for (int i = 0; i < len; i++) {
+        char symbol = inf[i];
+
+        if (symbol >= '0' && symbol <= '9') {
+            post += symbol;
+            post += ' ';
+        }
+        else if (symbol == '(') {
+            stack.push(symbol);
+        }
+        else if (symbol == ')') {
+            while (stack.get() != '(') {
+                post += stack.get();
+                post += ' ';
+                stack.pop();
+            }
+            stack.pop();
+        }
+        else {
+            while (!stack.isEmpty() && priority(stack.get()) >= priority(symbol)) {
+                post += stack.get();
+                post += ' ';
+                stack.pop();
+            }
+            stack.push(symbol);
+        }
     }
 
-  const T& pop() {
-    if (isEmpty()) {
-      throw std::string("Stack is empty");
-    } else {
-      return arr[top--];
-      }
+    while (!stack.isEmpty()) {
+        post += stack.get();
+        post += ' ';
+        stack.pop();
     }
 
-  bool isEmpty()const {
-    return top == -1;
-  }
+    return post;
+}
 
-  bool isFull()const {
-    return top == size - 1;
-  }
 
-  const T& get()const {
-    return arr[top];
-  }
-};
-#endif  // INCLUDE_TSTACK_H_
+int eval(std::string post) {
+    TStack<int, 100> stack;
+    int len = post.length();
+
+    for (int i = 0; i < len; i++) {
+        char symbol = post[i];
+
+        if (symbol >= '0' && symbol <= '9') {
+            int num = 0;
+
+            while (symbol >= '0' && symbol <= '9') {
+                num = num * 10 + (symbol - '0');
+                i++;
+                symbol = post[i];
+            }
+
+            stack.push(num);
+        }
+        else if (symbol != ' ') {
+            int operand2 = stack.get();
+            stack.pop();
+            int operand1 = stack.get();
+            stack.pop();
+
+            switch (symbol) {
+            case '+':
+                stack.push(operand1 + operand2);
+                break;
+            case '-':
+                stack.push(operand1 - operand2);
+                break;
+            case '*':
+                stack.push(operand1 * operand2);
+                break;
+            case '/':
+                stack.push(operand1 / operand2);
+                break;
+            }
+        }
+    }
+
+    return stack.get();
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::string infixExpression = "(2+2)*2";
+    std::string postfixExpression = infx2pstfx(infixExpression);
+
+    std::cout << "Infix выражение: " << infixExpression << std::endl;
+    std::cout << "Postfix выражение: " << postfixExpression << std::endl;
+
+    int result = eval(postfixExpression);
+    std::cout << "Результат: " << result << std::endl;
+
+    return 0;
+}
